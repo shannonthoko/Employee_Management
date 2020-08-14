863d37ae6633130c3ab4288bafaff35880b08339
Shannon Kearney <shannon.kearney@colorado.edu>
2020-08-13 19:06:18 -0600

    react app

---
+++ src/components/Container.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import SearchForm from "./SearchForm";
+import ResultList from "./Result";
+import API from "../utils/API";
+
+class Container extends Component {
+  state = {
+    search: "",
+    results: []
+  };
+
+  // When this component mounts, search the Giphy API for pictures of kittens
+  componentDidMount() {
+    this.searchGiphy("50");
+  }
+  
+  searchGiphy = query => {
+    API.search(query)
+    .then(res => this.setState({ results: res.data.results}))
+    .catch(err => console.log(err));
+    console.log(this.state.results);
+  };
+
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // When the form is submitted, search the Giphy API for `this.state.search`
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchGiphy(this.state.search);
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchForm
+          search={this.state.search}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
+        <ResultList results={this.state.results} />
+      </div>
+    );
+  }
+}
+
+export default Container;
---
+++ src/components/Result.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Result(props) {
+  return (
+   
+    <ul className="list-group">
+      {props.results.map(result => (
+        <li className="list-group-item" key={result.id}>
+          <img alt={result.name} className="img-fluid" src={result.picture.thumbnail} />
+          <p>Name: {result.name.first} {result.name.last}</p>
+          <p>Email: {result.email}</p>
+        </li>
+      ))}
+    </ul>
+
+    // <li> Name: <img alt={results.name} className="img-fluid" src={props.picture} /> </li>
+  );
+}
+
+export default Result;
